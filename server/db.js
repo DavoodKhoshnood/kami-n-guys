6ca3f764f93c6e6d6a6caa11a8a088df51a4e4aa
Saman <saman.zahedi10@gmail.com>
2022-08-12 22:53:45 +0100

    Modify db.js

--- server/db.js
+++ server/db.js
@@ -1,14 +1,11 @@
 import { Pool } from "pg";
 
-import config from "./utils/config";
-import logger from "./utils/logger";
+const dbUrl = process.env.DATABASE_URL;
 
 const pool = new Pool({
-	connectionString: config.dbUrl,
+	connectionString: dbUrl,
 	connectionTimeoutMillis: 5000,
-	ssl: config.dbUrl.includes("localhost")
-		? false
-		: { rejectUnauthorized: false },
+	ssl: dbUrl.includes("localhost") ? false : { rejectUnauthorized: false },
 });
 
 export const connectDb = async () => {
@@ -16,22 +13,16 @@ export const connectDb = async () => {
 	try {
 		client = await pool.connect();
 	} catch (err) {
-		logger.error("%O", err);
+		console.error(err);
 		process.exit(1);
 	}
-	logger.info("Postgres connected to %s", client.database);
+	console.log("Postgres connected to %s", client.database);
 	client.release();
 };
 
 export const disconnectDb = () => pool.close();
 
-/**
- * Access this with `import db from "path/to/db";` then use it with
- * `await db.query("<SQL>", [...<variables>])`.
- */
-export default {
-	query: (...args) => {
-		logger.debug("Postgres querying %O", args);
-		return pool.query.apply(pool, args);
-	},
-};
+export default { query:pool.query.bind(pool), pool };
+
+
+
