adebeddb2b15d842d83184273e1deb5306bb96f9
Saman <saman.zahedi10@gmail.com>
2022-08-13 13:45:20 +0100

    Test queries with starter kit

--- server/api.js
+++ server/api.js
@@ -1,12 +1,31 @@
 import { Router } from "express";
+import pool from "./db";
 
-import logger from "./utils/logger";
+// import logger from "./utils/logger";
 
 const router = Router();
 
 router.get("/", (_, res) => {
-	logger.debug("Welcoming everyone...");
+	console.log("Welcoming everyone...");
 	res.json({ message: "Hello, world!" });
 });
+
+
+router.get("/cars/:carsId", (req, res) => {
+	const id = req.params.lessonId;
+	pool
+		.query(`select * from cars where id = $1`, [id])
+		.then((result) => res.status(200).json(result.rows))
+		.catch((error) => res.status(500).json(error));
+});
+
+router.get("/cars", (req, res) => {
+	pool
+		.query(`select * from cars order by id`)
+		.then((result) => res.status(200).json(result.rows))
+		.catch((error) => res.status(500).json(error));
+});
+
+
 
 export default router;
